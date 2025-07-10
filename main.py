c898d4b4414e819b71dd8cea3e27a5fe11b1a962
JanMa <jan10markus@gmail.com>
2025-07-10 03:15:45 +0300

    Style, started edit menu, json structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-import sv_ttk
+#import sv_ttk
 import json
 import sys
 #from PIL import Image, ImageTk
@@ -17,17 +17,51 @@ def stop_app(rect, dirs):
 class App:
     def __init__(self, root, settings):
         self.root = root
-        self.frame = tk.Canvas(root, width=settings.get('dimensions')[0], height=settings.get('dimensions')[1], bg='#1c1c1c')
-        self.frame.pack()
-        self.frame.pack_propagate(False)
+        self.settings = settings
 
-        label = ttk.Label(self.frame, padding=[0,10,0,10], text='Select a game:')
-        label.pack()
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        frame = tk.Frame(root, bg='#1c1c1c')
+        frame.grid()
+        #self.frame.pack_propagate(False)
+        label = ttk.Label(frame, padding=[0,10,0,10], text='Select a game:')
+        label.pack(fill='both', expand=True)
 
         games = find_games()
         for game in games.keys():
-            button = ttk.Button(self.frame, padding=[0,5,0,5], text=game, command=lambda g=game: load_game(games[g]))
-            button.pack()
+            button = ttk.Button(frame, text=game, command=lambda g=game: self.load_game(games[g]))
+            button.pack(fill='both', expand=True)
+
+    def load_game(self, path):
+        game_data_raw = open(path, 'r', encoding='utf-8')
+        game_data = json.load(game_data_raw)
+
+        self.clear_screen()
+
+        frame = tk.Canvas(root, bg='#1c1c1c')
+        frame.grid(row=0, column=0, sticky='nsew')
+
+        for i in range(0, 5):
+            frame.rowconfigure(i, weight=1)
+            frame.columnconfigure(i, weight=1)
+
+        i = 0
+        for category in game_data['categories'].keys():
+            print(category)
+            button = ttk.Button(frame, text=category)
+            button.configure(style='question.TButton')
+            button.grid(column=i, row=0, sticky='n')
+            j = 1
+            for question in game_data['categories'][category]:
+                button = ttk.Button(frame, text=question)
+                button.grid(column=i, row = j, sticky='n')
+                j+=1
+            i+=1
+
+    def clear_screen(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
 
 def find_games():
     games = {}
@@ -36,11 +70,22 @@ def find_games():
             games[file[:-5]] = './games/' + file
     return games
 
-def load_game(path):
-    game_data_raw = open(path, 'r', encoding='utf-8')
-    game_data = json.load(game_data_raw)
-    print(path)
-    print(game_data['categories'])
+def setup_styles():
+    style = ttk.Style()
+    style.theme_use('clam')
+    style.layout('question.TButton', style.layout('Accent.TButton'))
+    style.configure('question.TButton', font=('Arial', 12), background="#595959", foreground="#fafafa", accent="#57c8ff")
+    style.map('TButton',
+              background=[
+                  ('active', '#2f60d8'),
+                  ('pressed', '#ffffff')
+              ])
+    style.configure('TButton', font=('Arial', 10), background="#595959", foreground="#fafafa", accent="#57c8ff",
+                    relief='solid',
+                    borderwidth=20,
+                    bordercolor='#57c8ff'
+                    )
+    style.configure('TLabel', font=('Arial', 10), background="#595959", foreground="#fafafa", accent="#57c8ff")
 
 if __name__ == '__main__':
     # Ignore first arg, since that is the name of the program
@@ -50,9 +95,10 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.iconbitmap('hakillak.ico')
     root.title('Hakillak')
+    setup_styles()
 
     settings = {'dimensions': [1000, 500]}
 
-    sv_ttk.set_theme('dark')
+    #sv_ttk.set_theme('dark')
     app = App(root, settings)
     root.mainloop()
